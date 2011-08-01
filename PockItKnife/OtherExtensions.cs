1ebe571827d3c01ec642fe0600a366eacb29b754
Christian Peterek <christian.peterek@brainlab.com>
2011-08-01 16:25:37 +0200

    Added a ResourceFile Reader

---
+++ PockItKnife/OtherExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    /// Class that contains single extensions for several objects.
+    /// </summary>
+    public static class OtherExtensions
+    {
+        /// <summary>
+        /// Loads an Embedded Resource File from a given assembly. The method looks for files compiled into the assembly ending with the 
+        /// given fileNameInAssembly. If multiple files with the same name exist, this returns the first in the list.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="fileNameInAssembly"></param>
+        /// <returns></returns>
+        /// <exception cref="System.IO.FileNotFoundException">Throws a FileNotFoundException if the file is not compiled into the assembly as Embedded Resource</exception>
+        public static string LoadEmbeddedFile(this System.Reflection.Assembly assembly, string fileNameInAssembly) {
+
+            var resName = from name in assembly.GetManifestResourceNames()
+                          where name.ToLower().EndsWith(fileNameInAssembly.ToLower())
+                          select name;
+
+            string text = default(string);
+            var fullResourceName = resName.FirstOrDefault();
+
+            if (fullResourceName == null)
+                throw new System.IO.FileNotFoundException("Could not find file {0} in the resources of assembly {1}.".Inject(fileNameInAssembly, assembly.FullName));
+
+            using (System.IO.Stream stream = assembly.GetManifestResourceStream(fullResourceName))
+            {
+                Byte[] bytes = new Byte[stream.Length];
+
+                stream.Read(bytes, 0, (int)(stream.Length));
+                text = System.Text.UTF8Encoding.Default.GetString(bytes);
+            }
+
+            return text;
+        }
+    }
+}
---
+++ PockItKnifeTest/OtherExtensionsTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using FluentAssertions;
+namespace PockItKnifeTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for OtherExtensionsTest and is intended
+    ///to contain all OtherExtensionsTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class OtherExtensionsTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod]
+        [ExpectedException(typeof(System.IO.FileNotFoundException))]
+        public void LoadEmbeddedFile__throws_file_not_found_exception()
+        {
+            //ARRANGE
+
+            //ACT
+            var s = Assembly.GetExecutingAssembly().LoadEmbeddedFile("thisfiledoesnotexist");
+
+            //ASSERT
+        }
+
+        [TestMethod]
+        public void LoadEmbeddedFile__reads_file_contents()
+        {
+            //ARRANGE
+
+            //ACT
+            var s = Assembly.GetExecutingAssembly().LoadEmbeddedFile("HELP");
+
+            //ASSERT
+            s.Should().Contain("Help!!");
+        }
+
+        [TestMethod]
+        public void LoadEmbeddedFile__reads_file_contents_with_extension()
+        {
+            //ARRANGE
+
+            //ACT
+            var s = Assembly.GetExecutingAssembly().LoadEmbeddedFile("HELP.txt");
+
+            //ASSERT
+            s.Should().Contain("Help!!");
+        }
+    }
+}
